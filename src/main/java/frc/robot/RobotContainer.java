26e61733265155f78859f0c3ab61f6d0277659c1
fastassonic <Gabcall04@gmail.com>
2021-12-07 16:51:23 -0600

    Added Command and made it default for the camera subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.CameraDisplay;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.Camera;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -21,9 +23,12 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final Camera m_camera = new Camera(); 
+  private final CameraDisplay m_CameraDisplay = new CameraDisplay(m_camera);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    m_camera.setDefaultCommand(m_CameraDisplay);
     // Configure the button bindings
     configureButtonBindings();
   }
--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -1,4 +1,4 @@
-// Copyright (c) FIRST and other WPILib contributors.
+ // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
@@ -8,13 +8,19 @@ import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Camera extends SubsystemBase {
+  CameraServer M_CameraServer;
   /** Creates a new Camera. */
   public Camera() {
-    CameraServer.getInstance().startAutomaticCapture();
+   M_CameraServer = CameraServer.getInstance();
+  }
+
+  public void smartDisplay() { 
+    M_CameraServer.startAutomaticCapture();
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+
+    
   }
 }
